40732db3a992973b20fd8e90b0125bdaf7b1f01a
umar.alfaruq.devid@gmail.com <umar.alfaruq.devid@gmail.com>
2019-10-03 19:55:18 +0700

    initializing

---
+++ application/controllers/loginController.php
@@ -0,0 +1,17 @@
+<?php
+	/**
+	 * Ini adalah controller untuk memenajemen login
+	 */
+	class loginController extends CI_Controller
+	{
+		
+		public function __construct()
+		{
+			parent::__construct();
+		}
+
+		public function index() {
+			echo "string";
+		}
+	}
+?>
