42dd12fd9ad10f9ae18e020bc8a8f240c92e959d
umar.alfaruq.devid@gmail.com <umar.alfaruq.devid@gmail.com>
2019-10-05 11:13:42 +0700

    cek apakah inputanjika gagal memenuhi peraturan maka kembali ke halaman login dan menampilkan error messages

--- application/controllers/loginController.php
+++ application/controllers/loginController.php
@@ -8,6 +8,7 @@
 		public function __construct()
 		{
 			parent::__construct();
+			$this->load->library('form_validation');
 		}
 
 		public function index() {
@@ -17,7 +18,27 @@
 		public function loginAct() {
 			$user = $this->input->post('user');
 			$pass = $this->input->post('pass');
-			var_dump($_POST);
+
+			/**
+			 * set pesan
+			 */
+
+			$this->form_validation->set_message('required', '{field} tidak boleh kosong');
+			$this->form_validation->set_message('alpha_numeric', '{field} hanya angka dan huruf');
+			$this->form_validation->set_message('min_length', '{field} minimal {param} karakter');
+			$this->form_validation->set_message('is_unique', '{field} sudah di gunakan');
+			$this->form_validation->set_message('matches', '{field} tidak sama dengan {param}');
+
+			 /**
+			 * set peraturan validasi
+			 */
+			$this->form_validation->set_rules('user', 'Username', 'required|alpha_numeric|min_length[8]');
+			$this->form_validation->set_rules('pass', 'Password', 'required|alpha_numeric|min_length[8]');
+			if($this->form_validation->run() == false) {
+				$this->load->view('auth/login');
+			} else {
+				echo "true";
+			}
 		}
 
 	}
