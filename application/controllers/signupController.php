8322cbd2701650be044498ef48f135806ae2ff41
umar.alfaruq.devid@gmail.com <umar.alfaruq.devid@gmail.com>
2019-10-05 17:13:05 +0700

    jika tidak memenuhi syarat

--- application/controllers/signupController.php
+++ application/controllers/signupController.php
@@ -8,11 +8,43 @@
 		public function __construct()
 		{
 			parent::__construct();
+			$this->load->library('form_validation');
 		}
 
 		public function index() 
 		{
 			$this->load->view('auth/signup');
+		}
+
+		/**
+		 * aksi untuk memanajemen pendaftaran
+		 */
+		public function signupAct()
+		{
+			$user = $this->input->post('user');
+			$pass = $this->input->post('pass');
+
+			/**
+			 * set pesan
+			 */
+
+			$this->form_validation->set_message('required', '{field} tidak boleh kosong');
+			$this->form_validation->set_message('alpha_numeric', '{field} hanya angka dan huruf');
+			$this->form_validation->set_message('min_length', '{field} minimal {param} karakter');
+			$this->form_validation->set_message('is_unique', '{field} sudah di gunakan');
+			$this->form_validation->set_message('matches', '{field} tidak sama dengan {param}');
+
+			 /**
+			 * set peraturan validasi
+			 */
+			$this->form_validation->set_rules('user', 'Username', 'required|alpha_numeric|min_length[8]|is_unique[accounts.username]');
+			$this->form_validation->set_rules('pass', 'Password', 'required|alpha_numeric|min_length[8]');
+			$this->form_validation->set_rules('retype', 'Ulang password', 'required|alpha_numeric|matches[pass]');
+			if($this->form_validation->run() == false) {
+				$this->load->view('auth/signup');
+			} else {
+				echo "true";
+			}
 		}
 
 	}
