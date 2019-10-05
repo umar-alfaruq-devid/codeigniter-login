66cc16cbdabe4675dff9c1161b1bd1319503e316
umar.alfaruq.devid@gmail.com <umar.alfaruq.devid@gmail.com>
2019-10-05 14:53:29 +0700

    first commit

---
+++ application/controllers/signupController.php
@@ -0,0 +1,19 @@
+<?php
+	/**
+	 * Ini adalah kontroler untuk memanejemen pendaftaran
+	 */
+	class signupController extends CI_Controller
+	{
+		
+		public function __construct()
+		{
+			parent::__construct();
+		}
+
+		public function index() 
+		{
+			
+		}
+
+	}
+?>
